5da61ea13252843f7114a7005661d7d7a4e9deda
Daniel Walker <nickeldan@gmail.com>
2022-07-10 23:04:21 -0400

    initial commit

---
+++ include/gear/gear.h
@@ -0,0 +1,46 @@
+#ifndef GENERAL_ARRAY_H
+#define GENERAL_ARRAY_H
+
+#include <sys/types.h>
+
+#include "config.h"
+
+#define GEAR_VERSION "0.0.1"
+
+typedef struct gear {
+    void *data;
+    size_t (*expander)(size_t);
+    size_t item_size;
+    size_t length;
+    size_t capacity;
+} gear;
+
+enum gearRetValue {
+    GEAR_RET_OK,
+    GEAR_RET_BAD_USAGE,
+    GEAR_RET_OUT_OF_MEMORY,
+    GEAR_RET_NO_EXPANSION,
+};
+
+size_t
+gearDefaultExpander(size_t capacity);
+
+#define GEAR_INIT(type)                               \
+    (gear)                                            \
+    {                                                 \
+        NULL, gearDefaultExpander, sizeof(type), 0, 0 \
+    }
+
+int
+gearAppend(gear *array, const void *item);
+
+void
+gearFree(gear *array);
+
+#define GEAR_GET_ITEM(array, idx) ((void *)((unsigned char *)(array)->data + (idx) * (array)->item_size))
+
+#define GEAR_FOR_EACH(array, item)                                            \
+    for (item = (array)->data; item != GEAR_GET_ITEM(array, (array)->length); \
+         item = (void *)((unsigned char *)item + (array)->item_size))
+
+#endif  // GENERAL_ARRAY_H
---
+++ src/gear.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "gear.h"
+
+size_t
+gearDefaultExpander(size_t capacity)
+{
+    size_t ret;
+
+    if (capacity == 0) {
+        return GEAR_DEFAULT_INIT_CAPACITY;
+    }
+    ret = capacity + GEAR_DEFAULT_EXPANSION;
+    // Check for integer overflow
+    return (ret < capacity) ? capacity : ret;
+}
+
+int
+gearAppend(gear *array, const void *item)
+{
+    if (!array || !item) {
+        return GEAR_RET_BAD_USAGE;
+    }
+
+    if (array->length == array->capacity) {
+        void *success;
+        size_t new_capacity;
+
+        new_capacity = array->expander(array->capacity);
+        if (new_capacity <= array->capacity) {
+            return GEAR_RET_NO_EXPANSION;
+        }
+
+        success = realloc(array->data, array->item_size * new_capacity);
+        if (!success) {
+            return GRR_RET_OUT_OF_MEMORY;
+        }
+        array->data = success;
+        array->capacity = new_capacity;
+    }
+
+    memcpy((unsigned char *)array->data + array->item_size * array->length, item, array->item_size);
+    array->length++;
+
+    return GEAR_RET_OK;
+}
+
+void
+gearFree(gear *array)
+{
+    if (!array) {
+        return;
+    }
+
+    free(array->data);
+    array->data = NULL;
+    arracy->length = array->capacity = 0;
+}
