636c754a5df9da66ac6f9280df35c5139d96d95b
Daniel Walker <nickeldan@gmail.com>
2022-08-18 22:43:28 -0400

    added the BUILD_DEPS variable to make.mk

--- make.mk
+++ make.mk
@@ -6,19 +6,21 @@ GEAR_STATIC_LIBRARY := $(GEAR_LIB_DIR)/libgear.a
 
 GEAR_SOURCE_FILES := $(wildcard $(GEAR_DIR)/src/*.c)
 GEAR_OBJECT_FILES := $(patsubst $(GEAR_DIR)/src/%.c,$(GEAR_OBJ_DIR)/%.o,$(GEAR_SOURCE_FILES))
-GEAR_HEADER_FILES := $(wildcard $(GEAR_DIR)/include/gear/*.h) 
+GEAR_HEADER_FILES := $(wildcard $(GEAR_DIR)/include/gear/*.h)
 GEAR_INCLUDE_FLAGS := -I$(GEAR_DIR)/include
 
 GEAR_DEPS_FILE := $(GEAR_DIR)/deps.mk
 DEPS_FILES += $(GEAR_DEPS_FILE)
 
-ifneq ($(MAKECMDGOALS),clean)
+BUILD_DEPS ?= $(if $(MAKECMDGOALS),$(subst clean,,$(MAKECMDGOALS)),yes)
+
+ifneq ($(BUILD_DEPS),)
 
 $(GEAR_DEPS_FILE): $(GEAR_SOURCE_FILES) $(GEAR_HEADER_FILES)
 	rm -f $@
 	for file in $(GEAR_SOURCE_FILES); do \
-	  echo "$(GEAR_OBJ_DIR)/`$(CC) $(GEAR_INCLUDE_FLAGS) -MM $$file`" >> $@ && \
-	  echo '\t$$(CC) $$(CFLAGS) -fpic -ffunction-sections $(GEAR_INCLUDE_FLAGS) -c $$< -o $$@' >> $@; \
+	    echo "$(GEAR_OBJ_DIR)/`$(CC) $(GEAR_INCLUDE_FLAGS) -MM $$file`" >> $@ && \
+	    echo '\t$$(CC) $$(CFLAGS) -fpic -ffunction-sections $(GEAR_INCLUDE_FLAGS) -c $$< -o $$@' >> $@; \
 	done
 include $(GEAR_DEPS_FILE)
 
